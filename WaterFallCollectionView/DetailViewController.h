d7ace051c53fd487667e1efbfd3f6c94c22375c7
PengLi <995139593@qq.com>
2014-09-19 10:42:45 +0800

    new
    新加

---
+++ WaterFallCollectionView/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  WaterFallCollectionView
+//
+//  Created by pg on 14-9-19.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ WaterFallCollectionView/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  WaterFallCollectionView
+//
+//  Created by pg on 14-9-19.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
